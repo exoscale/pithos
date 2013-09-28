52bcb11999946304cd082a8564c401d7219f9cf2
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-09-28 11:57:38 +0200

    wire in make bucket operation.

--- src/io/exo/pithos/operations.clj
+++ src/io/exo/pithos/operations.clj
@@ -1,16 +1,26 @@
 (ns io.exo.pithos.operations
-  (:require [io.exo.pithos.response :refer [response xml-response]]
+  (:require [io.exo.pithos.response :refer [header response
+                                            xml-response request-id]]
             [io.exo.pithos.store    :as store]
             [io.exo.pithos.bucket   :as bucket]
             [io.exo.pithos.xml      :as xml]))
 
 (defn get-service
   "lists all bucket"
-  [{{:keys [tenant]} :authorization} filestore]
+  [{{:keys [tenant]} :authorization :as request} filestore]
   (store/execute filestore
     (-> (bucket/fetch tenant)
         (xml/list-all-my-buckets)
-        (xml-response))))
+        (xml-response)
+        (request-id request))))
+
+(defn put-bucket
+  [{{:keys [tenant]} :authorization :keys [bucket] :as request} filestore]
+  (store/execute filestore (bucket/update! tenant bucket))
+  (-> (response)
+      (request-id request)
+      (header "Location" (str "/" bucket))
+      (header "Connection" "close")))
 
 (defn unknown
   "unknown operation"
@@ -19,7 +29,8 @@
       (xml-response)))
 
 (def opmap
-  {:get-service get-service})
+  {:get-service get-service
+   :put-bucket  put-bucket})
 
 (defn dispatch
   [{:keys [operation] :as request} filestore]
