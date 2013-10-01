665e369744d162a444e29aa50ad859e115c2c5b2
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-10-01 16:51:38 +0200

    try creating keyspaces when they are not found

--- src/io/exo/pithos/store.clj
+++ src/io/exo/pithos/store.clj
@@ -1,5 +1,6 @@
 (ns io.exo.pithos.store
-  (:require [qbits.alia :as alia]))
+  (:require [qbits.alia :as alia]
+            [qbits.hayt :refer [use-keyspace create-keyspace with]]))
 
 (defmacro execute
   [store & body]
@@ -7,6 +8,16 @@
      (do ~@body)))
 
 (defn cassandra-store
-  [{:keys [cluster keyspace]}]
-  (-> (alia/cluster cluster)
-      (alia/connect keyspace)))
+  "Connect to a cassandra cluster, and use a specific keyspace.
+   When the keyspace is not found, try creating it"
+  [{:keys [cluster keyspace hints]
+    :or {hints {:replication {:class             "SimpleStrategy"
+                              :replication_factor 1}}}}]
+  (let [session (-> (alia/cluster cluster) (alia/connect))]
+    (try (alia/execute session (use-keyspace keyspace))
+         session
+         (catch com.datastax.driver.core.exceptions.InvalidQueryException e
+           ;;
+           (if (re-find #"^[kK]eyspace.*does not exist$" (.getMessage e))
+             (alia/execute session (create-keyspace keyspace (with hints)))
+             (throw e))))))
