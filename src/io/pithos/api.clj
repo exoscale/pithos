b9fc127e0f28fd77500af66f2a73349b4ee10b02
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-10-02 16:59:32 +0200

    feed metastore to operation dispatching

--- src/io/pithos/api.clj
+++ src/io/pithos/api.clj
@@ -6,6 +6,8 @@
 
 (defn run
   [{:keys [keystore metastore regions service options]}]
-  (run-server #(-> % (prepare keystore metastore regions options) (dispatch regions))
+  (run-server #(-> %
+                   (prepare keystore metastore regions options)
+                   (dispatch metastore regions))
               service)
   (info "server up and running"))
