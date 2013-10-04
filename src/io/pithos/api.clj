548e68438a50e340dd0a817ce52435e876875779
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-10-04 02:10:09 +0200

    wire requests through aleph instead of httpkit

--- src/io/pithos/api.clj
+++ src/io/pithos/api.clj
@@ -1,13 +1,15 @@
 (ns io.pithos.api
-  (:require [org.httpkit.server    :refer [run-server]]
+  (:require [aleph.http            :as http]
+            [lamina.core           :refer [enqueue]]
             [clojure.tools.logging :refer [info]]
             [io.pithos.operations  :refer [dispatch]]
             [io.pithos.request     :refer [prepare]]))
 
 (defn run
   [{:keys [keystore bucketstore regions service options]}]
-  (run-server #(-> %
-                   (prepare keystore bucketstore regions options)
-                   (dispatch bucketstore regions))
-              service)
+  (let [handler (fn [chan request] 
+                  (-> (assoc request :chan chan)
+                      (prepare keystore bucketstore regions options)
+                      (dispatch bucketstore regions)))]
+    (http/start-http-server handler service))
   (info "server up and running"))
