091c9a6827d0f03e2aa12055ba3f65f882a601fb
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-10-16 13:39:40 +0200

    move main namespace to io.exo.pithos

---
+++ src/io/pithos.clj
@@ -0,0 +1,57 @@
+(ns io.pithos
+  "
+pithos: object storage daemon
+=============================
+
+Pithos is an object storage daemon with
+pluggable implementation of storage
+engines. See [pithos.io] for design details"
+  (:gen-class)
+  (:require [io.pithos.api     :as api]
+            [io.pithos.schema  :as schema]
+            [io.pithos.config  :as config]
+            [clojure.tools.cli :refer [cli]]))
+
+(defn get-action
+  "Figure out what the expected action is from the command-line."
+  [action]
+  (let [amap {"api-run"        api/run
+              "install-schema" schema/converge-schema}]
+    (or (get amap action)
+        (do (println "unknown action: " action)
+            (System/exit 1)))))
+
+(defn get-cli
+  "Parse command line arguments and ensure we return a proper structure."
+  [args]
+  (try
+    (-> (cli args
+             ["-h" "--help" "Show Help"
+              :default false :flag true]
+             ["-f" "--path" "Configuration file path"
+              :default nil]
+             ["-q" "--quiet" "Never output to stdout"
+              :default false :flag true]
+             ["-a" "--action" "Specify an action (api-run, install-schema)"
+              :default "api-run"])
+        (update-in [0 :action] get-action))
+    (catch Exception e
+      (println "Could not parse arguments: " (.getMessage e))
+      (System/exit 1))))
+
+(defn -main 
+  "Main startup path, parse command line arguments, then dispatch to
+   appropriate action.
+
+   Only two actions are available:
+
+     - `api-run`: run the S3 api handler
+     - `install-schema`: converge cassandra schema"
+  [& args]
+  (let [[{:keys [path help action quiet]} args banner] (get-cli args)]
+    (when help
+      (println banner)
+      (System/exit 0))
+    (-> path (config/init quiet) action))
+  nil)
+
