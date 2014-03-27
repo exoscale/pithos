95356a55d9ee5c600d47ca716eafc4954a73a4fc
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-03-27 17:13:41 +0100

    document bucketstore

--- src/io/pithos/bucket.clj
+++ src/io/pithos/bucket.clj
@@ -1,4 +1,7 @@
 (ns io.pithos.bucket
+  "The bucketstore stores ring-global information on bucket
+   ownership. It contains a single column-family and an
+   accompanying index."
   (:require [qbits.alia      :refer [execute with-session]]
             [qbits.hayt      :refer [select where set-columns
                                      create-table create-index
@@ -8,6 +11,8 @@
             [io.pithos.store :as store]))
 
 (defprotocol Bucketstore
+  "The bucketstore contains the schema migration function,
+   two bucket lookup functions and CRUD signatures"
   (converge! [this])
   (by-tenant [this tenant])
   (by-name [this bucket])
@@ -16,45 +21,65 @@
   (delete! [this bucket]))
 
 ;; ring-global metastore
+
 (def bucket-table
-    (create-table
-     :bucket
-     (column-definitions {:bucket       :text
-                          :created      :text
-                          :tenant       :text
-                          :region       :text
-                          :acl          :text
-                          :cors         :text
-                          :website      :text
-                          :policy       :text
-                          :versioned    :boolean
-                          :primary-key  :bucket})))
+  "Bucket properties"
+  (create-table
+   :bucket
+   (column-definitions {:bucket       :text
+                        :created      :text
+                        :tenant       :text
+                        :region       :text
+                        :acl          :text
+                        :cors         :text
+                        :website      :text
+                        :policy       :text
+                        :versioned    :boolean
+                        :primary-key  :bucket})))
 
 (def bucket_tenant-index
-    (create-index
-     :bucket
-     :tenant
-     (index-name :bucket_tenant)))
+  "Index bucket on tenant"
+  (create-index
+   :bucket
+   :tenant
+   (index-name :bucket_tenant)))
 
 (defn bucket-by-tenant-q
+  "Cassandra query for bucket by tenant"
   [tenant]
   (select :bucket (where {:tenant tenant})))
 
 (defn fetch-bucket-q
+  "Cassandra query for bucket by name"
   [bucket]
   (select :bucket (where {:bucket bucket}) (limit 1)))
 
 (defn update-bucket-q
+  "Bucket creation or update"
   [bucket columns]
   (update :bucket
           (set-columns columns)
           (where {:bucket bucket})))
 
 (defn delete-bucket-q
+  "Bucket destruction"
   [bucket]
   (delete :bucket (where {:bucket bucket})))
 
 (defn cassandra-bucket-store
+  "Given a cluster configuration, reify an instance of Bucketstore.
+   The cassandra bucket store suffers from a design flaw since last
+   write-wins might yield a success response for a bucket which will
+   later be claimed.
+
+   This can be fixed with the following strategies:
+
+   - writing a bucket store that targets an SQL DB instead of cassandra
+   - using lightweight transactions
+   - wrap ownership around a zookeeper lock
+
+   If you care deeply about bucket ownership, I'd suggest looking into
+   the above options"
   [{:keys [default-region] :as config}]
   (let [session (store/cassandra-store config)]
     (reify Bucketstore
