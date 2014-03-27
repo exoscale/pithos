bdab32842b35908d4ea9ebe252f261e5618c9a01
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-03-27 17:46:15 +0100

    document keystore

--- src/io/pithos/keystore.clj
+++ src/io/pithos/keystore.clj
@@ -1,8 +1,19 @@
-(ns io.pithos.keystore)
+(ns io.pithos.keystore
+  "A keystore is a simple protocol which yields a map
+   of tenant details for a key id.
+
+   The basic implementation wants keys from the configuration
+   file, you'll likely want to use a custom implementation that
+   interacts with your user-base here.
+")
 
 (defprotocol Keystore
+  "Single function protocol, fetch retrieves information for an ID"
   (fetch [this id]))
 
+;;
+;; A simple record holding onto a map of keys to details.
+;; the fetch fn just looks up in its map.
 (defrecord MapKeystore [keys]
   Keystore
   (fetch [this id]
