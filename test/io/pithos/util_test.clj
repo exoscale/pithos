302ab9b56f4e20476bb0e603f1feb3839ae479ba
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-05-14 09:07:56 +0200

    add a test for to-bytes

--- test/io/pithos/util_test.clj
+++ test/io/pithos/util_test.clj
@@ -1,6 +1,6 @@
 (ns io.pithos.util-test
   (:require [clojure.test   :refer :all]
-            [io.pithos.util :refer [inc-prefix]]))
+            [io.pithos.util :refer [inc-prefix to-bytes]]))
 
 (deftest inc-prefix-test
   (testing "nil prefix"
@@ -10,3 +10,22 @@
 
   (testing "simple prefix"
     (is (= "fop" (inc-prefix "foo")))))
+
+(deftest byte-factor-test
+  (testing "from bytes"
+    (is (= 512 (to-bytes "512"))))
+
+  (testing "from kilobytes"
+    (is (= 2048 (to-bytes "2k"))))
+
+  (testing "from megabytes"
+    (is (= 2097152 (to-bytes "2m"))))
+
+  (testing "from gigabytes"
+    (is (= 2147483648 (to-bytes "2G"))))
+
+  (testing "from terabytes"
+    (is (= 2199023255552 (to-bytes "2tb"))))
+
+  (testing "from petabytes"
+    (is (= 2251799813685248 (to-bytes "2Pb")))))
