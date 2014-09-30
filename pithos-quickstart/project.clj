da3cd6b0e4f8ce5ecf954836affe4f879a06fbf1
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-09-30 22:32:53 +0200

    sync with clojars available version

--- pithos-quickstart/project.clj
+++ pithos-quickstart/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main io.pithos.quickstart
   :dependencies [[org.clojure/clojure                "1.7.0-alpha2"]
-                 [io.pithos/pithos                   "0.1.9"]
+                 [io.pithos/pithos                   "0.1.14"]
                  [org.apache.cassandra/cassandra-all "2.1.0"]])
