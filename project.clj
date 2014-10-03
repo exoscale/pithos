1fbde9f4aec996879c4f48235a8e7ed7d2619947
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-10-03 15:17:45 +0200

    pre-compile namespaces

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :url "http://pithos.io"
   :license {:name "Apache License, Version 2.0"
             :url " http://www.apache.org/licenses/LICENSE-2.0"}
+  :aot :all
   :main io.pithos
   :jvm-opts ["-Xmx2g"]
   :profiles {:dev {:resource-paths ["test/data"]}}
