7ee56a1581a473eb39f6522a3386e25eb7102b75
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-10-21 16:30:35 +0200

    somewhat improve readability by moving to reduce.

--- src/io/pithos/stream.clj
+++ src/io/pithos/stream.clj
@@ -98,45 +98,46 @@
     (d/col! dst :checksum (d/checksum src))
     dst))
 
+(defn stream-copy-part-block
+  [notifier dst hash part g-offset block]
+  (let [dblob      (d/blobstore dst)
+        sblob      (d/blobstore part)
+        real-block (+ g-offset (:block block))]
+    (debug "streaming block: " (:inode block) (:block block))
+    (b/start-block! dblob dst (+ g-offset (:block block)) (:size block))
+    (notifier :block)
+    (last
+     (for [chunk (b/chunks sblob part (:block block) (:block block))
+           :let [offset      (:offset chunk)
+                 payload     (:payload chunk)
+                 real-offset (+ g-offset offset)]]
+       (do
+         (b/chunk! dblob dst real-block real-offset payload)
+         (notifier :chunk)
+         (let [pos (.position payload)
+               sz  (.remaining payload)
+               ba  (byte-array sz)]
+           (.get payload ba)
+           (.position payload pos)
+           (u/md5-update hash ba 0 sz)
+           (+ real-offset sz)))))))
+
+
+(defn stream-copy-part
+  [notifier dst [offset hash] part]
+  (let [sblob  (d/blobstore part)
+        blocks (b/blocks sblob part)]
+
+    (debug "streaming part: " (d/part part))
+    [(reduce (partial stream-copy-part-block notifier dst hash part) offset blocks)
+     hash]))
+
 (defn stream-copy-parts
+  "Given a list of parts, stream their content to a destination inode"
   [parts dst notifier]
   (let [dblob   (d/blobstore dst)
-        hash    (u/md5-init)
-        g-offset 0]
-    (loop [g-offset       0
-           [part & parts] parts]
-      (if part
-        (let [sblob  (d/blobstore part)
-              blocks (b/blocks sblob part)]
-          (debug "streaming part: " (d/part part))
-          (recur
-           (last
-            (for [{:keys [block]} blocks
-                  :let [g-block (+ g-offset block)]]
-              (do
-                (b/start-block! dblob dst g-block g-block)
-                (notifier :block)
-                (loop [offset block]
-                  (if-let [chunks (b/chunks sblob part block offset)]
-                    (do
-                      (doseq [chunk chunks
-                              :let [offset      (:offset chunk)
-                                    payload     (:payload chunk)
-                                    real-offset (+ g-offset offset)]]
-                        (b/chunk! dblob dst g-block real-offset payload)
-                        (notifier :chunk)
-                        (let [pos (.position payload)
-                              sz  (.remaining payload)
-                              ba  (byte-array sz)]
-                          (.get payload ba)
-                          (.position payload pos)
-                          (u/md5-update hash ba 0 sz)))
-                      (let [{:keys [offset chunksize]} (last chunks)]
-                        (recur (+ offset chunksize))))
-                    (+ offset g-offset))))))
-           parts))
-        (do
-          (d/col! dst :size g-offset)
-          (d/col! dst :checksum (u/md5-sum hash))
-          (debug "stored size:" g-offset "and checksum: " (u/md5-sum hash))
-          dst)))))
+        [size hash] (reduce (partial stream-copy-part notifier dst) [0 (u/md5-init)] parts)]
+    (d/col! dst :size size)
+    (d/col! dst :checksum (u/md5-sum hash))
+    (debug "stored size:" size "and checksum: " (u/md5-sum hash))
+    dst))
