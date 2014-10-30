a88416af1120b02a12a2588cf7ba3ceccfdf1a5e
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-10-30 14:26:46 +0100

    allow level to be set in the reporter

--- src/io/pithos/reporter.clj
+++ src/io/pithos/reporter.clj
@@ -1,14 +1,14 @@
 (ns io.pithos.reporter
-  (:require [clojure.tools.logging :refer [info]]))
+  (:require [clojure.tools.logging :refer [log]]))
 
 (defprotocol Reporter
   (report! [this event]))
 
 (defn log4j-reporter
-  []
+  [{:keys [level]}]
   (reify Reporter
     (report! [_ event]
-      (info (pr-str event)))))
+      (log (keyword level) (pr-str event)))))
 
 (defn report-all!
   [reporters event]
