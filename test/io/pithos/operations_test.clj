2a5e0e26741fa477146d4c773a5493157ac166bb
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-01 12:50:53 +0100

    add test for endless range

--- test/io/pithos/operations_test.clj
+++ test/io/pithos/operations_test.clj
@@ -5,10 +5,14 @@
 
 (deftest range-test
 
-  (let [desc   (reify desc/BlobDescriptor (size [this] :nope))
-        in-out ["no headers"    {}                                  [0  :nope]
+  (let [desc   (reify desc/BlobDescriptor (size [this] 1024))
+        in-out ["no headers"    {}                                  [0  1024]
                 "range"         {"range" "bytes=10-90"}             [10 90]
-                "content-range" {"content-range" "bytes 10-90/200"} [10 90]]]
+                "content-range" {"content-range" "bytes 10-90/200"} [10 90]
+                "begin-range1"  {"range" "bytes=10-"}               [10 1024]
+                "begin-range2"  {"content-range" "bytes 10-/200"}   [10 1024]
+                "priority"      {"range"         "bytes=1-"
+                                 "content-range" ""}                [1 1024]]]
     (doseq [[nickname headers output] (partition 3 in-out)]
       (testing (str "valid output for " nickname)
         (is (= output (get-range desc headers)))))))
