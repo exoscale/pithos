90aac5b89b1b27dff8689aae4d8d38c6ff9db5f0
Vincent Bernat <Vincent.Bernat@exoscale.ch>
2014-11-20 13:56:51 +0100

    util: provide a string->pattern function
    This function will be used when we want to include plain strings into
    regular patterns.

--- test/io/pithos/util_test.clj
+++ test/io/pithos/util_test.clj
@@ -1,6 +1,6 @@
 (ns io.pithos.util-test
   (:require [clojure.test   :refer :all]
-            [io.pithos.util :refer [inc-prefix to-bytes parse-uuid]]))
+            [io.pithos.util :refer [inc-prefix to-bytes parse-uuid string->pattern]]))
 
 (deftest inc-prefix-test
   (testing "nil prefix"
@@ -34,3 +34,25 @@
   (testing "from uuid"
     (is (= #uuid "05ac767e-170f-a639-1ce7-39078945ee4480"
            (parse-uuid "05ac767e-170f-a639-1ce7-39078945ee4480")))))
+
+(deftest string-to-pattern-test
+  (testing "no special characters"
+    (is (= "17hj018" (string->pattern "17hj018"))))
+  (testing "single character"
+    (is (= "/" (string->pattern "/"))))
+  (testing "with dots and stars"
+    (is (= "\\.\\*89\\+2\\?" (string->pattern ".*89+2?"))))
+  (testing "with grouping"
+    (is (= "\\(89\\)" (string->pattern "(89)"))))
+  (testing "with anchors"
+    (is (= "\\^test\\$" (string->pattern "^test$"))))
+  (testing "with classes"
+    (is (= "\\\\d\\\\s\\\\S" (string->pattern "\\d\\s\\S"))))
+  (testing "with sets and repetitions"
+    (is (= "\\[a\\-z\\]\\{1,2\\}" (string->pattern "[a-z]{1,2}"))))
+  (testing "with alternatives"
+    (is (= "abc\\|cde" (string->pattern "abc|cde"))))
+  (testing "with escapes"
+    (is (= "abc\\\\" (string->pattern "abc\\"))))
+  (testing "with back references"
+    (is (= "\\(42\\)\\\\1\\\\k\\<here\\>" (string->pattern "(42)\\1\\k<here>")))))
