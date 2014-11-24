5b30d6e10af8f2d0a17722045e4d1558d9a3f0c1
Vincent Bernat <Vincent.Bernat@exoscale.ch>
2014-11-24 11:36:21 +0100

    http: do not use white spaces to keep the connection alive
    When sending either `\n` or ` ` to keep the HTTP connection alive, the
    answer is also padded with those characters. An XML answer cannot start
    with a whitespace. If there is an XML declaration, it must be first.
    
    Boto is using Python XML SAX parser which doesn't like whitespaces. XML
    parser in PHP doesn't like it either.
    
    Unfortunately, with this change, Jetty doesn't send an empty chunk, it
    doesn't send anything. So, this breaks the keepalived mechanism.

--- src/io/pithos/operations.clj
+++ src/io/pithos/operations.clj
@@ -512,7 +512,7 @@
   (let [ch        (chan)
         previous  (desc/init-version od)
         push-str  (fn [type]
-                    (put! ch (case type :block "\n" :chunk " " type)))
+                    (put! ch (case type :block "" :chunk "" type)))
         etag      (promise)
         details   (meta/get-upload-details (bucket/metastore od)
                                            bucket object upload-id)
