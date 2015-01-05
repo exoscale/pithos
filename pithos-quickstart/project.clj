7effb7aa39a8730fbd0ebe5bd86213c9025a8003
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-01-05 09:58:35 +0100

    release 0.7.3

--- pithos-quickstart/project.clj
+++ pithos-quickstart/project.clj
@@ -1,10 +1,10 @@
-(defproject io.pithos/pithos-quickstart "0.7.2-SNAPSHOT"
+(defproject io.pithos/pithos-quickstart "0.7.3"
   :description "pithos and cassandra, bundled together"
   :url "http://pithos.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main io.pithos.quickstart
-  :dependencies [[io.pithos/pithos                   "0.7.1"]
+  :dependencies [[io.pithos/pithos                   "0.7.3"]
                  [org.apache.cassandra/cassandra-all "2.1.1"
                   :exclusions [org.slf4j/slf4j-api
                                org.slf4j/slf4j-log4j12]]])
