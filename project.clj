d56f875bc62ba07235ff844dd27e9897659395d9
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-01-05 09:58:08 +0100

    release 0.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.pithos/pithos "0.7.2-SNAPSHOT"
+(defproject io.pithos/pithos "0.7.3"
   :description "cassandra-backed object storage"
   :maintainer {:email "Pierre-Yves Ritschard <pyr@spootnik.org>"}
   :url "http://pithos.io"
