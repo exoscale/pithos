69b1651ee619759c974e75de12e265bd084fa505
Christian Schröder <christian.schroeder@conceptboard.com>
2015-03-30 10:31:05 +0200

    parse more valid RFC822 dates

--- src/io/pithos/util.clj
+++ src/io/pithos/util.clj
@@ -5,7 +5,8 @@
            [java.lang              Math]
            [org.jboss.netty.buffer ChannelBuffers])
   (:require [clojure.string  :refer [lower-case]]
-            [clj-time.format :refer [formatters parse unparse formatter]]))
+            [clj-time.core   :as time-core]
+            [clj-time.format :refer [formatters parse unparse formatter with-zone]]))
 
 (defn md5-init
   "Yield an MD5 MessageDigest instance"
@@ -83,12 +84,74 @@
   []
   (javax.xml.bind.DatatypeConverter/printDateTime (Calendar/getInstance @utc)))
 
+
+(defn ^{:private true} formatter-with-zone
+  [s z]
+  (with-zone (formatter s) z))
+
+(def ^{:private true} rfc822-interop-format
+  "Common used date format, when rfc822 is supposed to be supported"
+  (formatter-with-zone "EEE, dd MMM yyyy HH:mm:ss 'GMT'" time-core/utc))
+
+(def ^{:private true} rfc822-obs-military-zones
+  "Map of military time zone identifiers to timezone"
+  (let [identifiers-upper "YXWVUTSRQPONZABCDEFGHIKLM"
+        identifiers-lower (.toLowerCase identifiers-upper java.util.Locale/US)
+        identifiers       (concat identifiers-upper identifiers-lower)
+        offsets           (range -12 13)
+        offset-zones      (map time-core/time-zone-for-offset offsets)]
+    (zipmap identifiers (cycle offset-zones))))
+
+(def ^{:private true} rfc822-obs-named-zones
+  "Map of RFC822 named time zone identifiers to timezone"
+  ;; this handles daylight saving times by trusting the correctness of the given string
+  ;; even if the date is given in the wrong tz (e.g. EDT in December)
+  {"UT"   time-core/utc
+   "GMT"  time-core/utc
+   "EST"  (time-core/time-zone-for-offset -5)
+   "EDT"  (time-core/time-zone-for-offset -4)
+   "CST"  (time-core/time-zone-for-offset -6)
+   "CDT"  (time-core/time-zone-for-offset -5)
+   "MST"  (time-core/time-zone-for-offset -7)
+   "MDT"  (time-core/time-zone-for-offset -6)
+   "PST"  (time-core/time-zone-for-offset -8)
+   "PDT"  (time-core/time-zone-for-offset -7)})
+
+
+(def ^{:private true} rfc822-formatters
+  "List of date formatters as specified in RFC822 (RFC2822) in order of preference"
+  (let [zone-mapping->formatter (fn [[zone offset]] (-> "EEE, dd MMM yyyy HH:mm:ss '%s'"
+                                                        (format zone)
+                                                        (formatter-with-zone offset)))]
+    (concat
+      ;; recommended date format of rfc822 successor rfc2822
+      [(formatter "EEE, dd MMM yyyy HH:mm:ss Z")]
+      [rfc822-interop-format]
+      ;; use joda time tz parsing;
+      ;; it handles summer time better if EST is given during EDT dates
+      ;; this is more lenient than RFC822 as it allows more time zone strings (e.g. UTC)
+      [(formatter "EEE, dd MMM yyyy HH:mm:ss z")]
+      (->> rfc822-obs-named-zones (map zone-mapping->formatter))
+      (->> rfc822-obs-military-zones (map zone-mapping->formatter)))))
+
+(defn ^{:private true} parse-with-formatter-safe
+  "Parses string with formatter and returns parse-error or nil"
+  ([s formatter] (parse-with-formatter-safe s formatter nil))
+  ([s formatter parse-error]
+  (try
+    (parse formatter s)
+    (catch IllegalArgumentException iae
+      parse-error))))
+
+
 (def rfc822-format
-  (formatter "EEE, dd MMM yyyy HH:mm:ss z"))
+  rfc822-interop-format)
 
 (defn rfc822->date
+  "Attempts to parse with all rfc822-formatters and returns the first non-nil result or throws an IllegalArgumentException."
   [s]
-  (parse rfc822-format s))
+  (or (some identity (map #(parse-with-formatter-safe s % nil) rfc822-formatters))
+      (throw (new IllegalArgumentException (format "Could not parse [ %s ] as RFC822 date" s)))))
 
 (defn iso8601->rfc822
   "RFC822 representation based on an iso8601 timestamp"
