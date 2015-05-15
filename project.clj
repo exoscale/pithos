0135ba6d62193ea65e13ff79d948e37f9a69f243
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-05-15 14:47:18 +0200

    project: bump dependencies

--- project.clj
+++ project.clj
@@ -10,9 +10,9 @@
   :profiles {:dev {:resource-paths ["test/data"]}}
   :plugins [[lein-sub      "0.3.0"]
             [lein-difftest "2.0.0"]
-            [lein-rpm "0.0.5"]]
+            [lein-ancient  "0.6.7"]]
   :sub ["pithos-quickstart"]
-  :dependencies [[org.clojure/clojure           "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure           "1.7.0-beta3"]
                  [org.clojure/data.codec        "0.1.0"]
                  [org.clojure/data.xml          "0.0.8"]
                  [org.clojure/data.zip          "0.1.1"]
@@ -22,12 +22,12 @@
                  [org.spootnik/logconfig        "0.7.3"]
                  [org.spootnik/constance        "0.5.2"]
                  [clj-yaml                      "0.4.0"]
-                 [clout                         "2.1.0"]
+                 [clout                         "2.1.2"]
                  [cheshire                      "5.4.0"]
                  [clj-time                      "0.9.0"]
                  [ring/ring-core                "1.3.2"]
                  [ring/ring-codec               "1.0.0"]
-                 [cc.qbits/alia                 "2.3.3"]
-                 [cc.qbits/jet                  "0.5.5"]
+                 [cc.qbits/alia                 "2.3.9"]
+                 [cc.qbits/jet                  "0.6.2"]
                  [net.jpountz.lz4/lz4           "1.3"]
-                 [org.xerial.snappy/snappy-java "1.1.1.6"]])
+                 [org.xerial.snappy/snappy-java "1.1.1.7"]])
