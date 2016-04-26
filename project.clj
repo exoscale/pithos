2700d830380e5d758720683ac2ed7c461510b76c
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-04-26 12:15:59 +0200

    project: bump dependencies

--- project.clj
+++ project.clj
@@ -28,7 +28,8 @@
                  [clj-time                      "0.9.0"]
                  [ring/ring-core                "1.3.2"]
                  [ring/ring-codec               "1.0.0"]
-                 [cc.qbits/alia                 "2.8.0"]
+                 [cc.qbits/alia                 "3.1.3"]
+                 [cc.qbits/hayt                 "3.0.1"]
                  [cc.qbits/jet                  "0.6.2"]
                  [net.jpountz.lz4/lz4           "1.3"]
                  [org.xerial.snappy/snappy-java "1.1.1.7"]])
