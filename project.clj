96b523f2ac4586992deb11ecfb3ce25094f44b64
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-05-25 11:26:42 +0200

    project: bring deps up to date.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject io.pithos/pithos "0.7.5"
+(defproject io.pithos/pithos "0.7.6-SNAPSHOT"
   :description "cassandra-backed object storage"
   :maintainer {:email "Pierre-Yves Ritschard <pyr@spootnik.org>"}
   :url "http://pithos.io"
   :license {:name "Apache License, Version 2.0"
-            :url " http://www.apache.org/licenses/LICENSE-2.0"}
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :aot :all
   :main io.pithos
   :jvm-opts ["-Xmx2g"]
@@ -13,14 +13,14 @@
             [lein-rpm "0.0.5"]
             [lein-ancient  "0.6.7"]]
   :sub ["pithos-quickstart"]
-  :dependencies [[org.clojure/clojure           "1.7.0"]
+  :dependencies [[org.clojure/clojure           "1.8.0"]
                  [org.clojure/data.codec        "0.1.0"]
                  [org.clojure/data.xml          "0.0.8"]
                  [org.clojure/data.zip          "0.1.1"]
-                 [org.clojure/tools.cli         "0.3.1"]
+                 [org.clojure/tools.cli         "0.3.5"]
                  [org.clojure/tools.logging     "0.3.1"]
-                 [org.clojure/core.async        "0.1.346.0-17112a-alpha"]
-                 [spootnik/unilog               "0.7.4"]
+                 [org.clojure/core.async        "0.2.374"]
+                 [spootnik/unilog               "0.7.13"]
                  [spootnik/constance            "0.5.3"]
                  [clj-yaml                      "0.4.0"]
                  [clout                         "2.1.2"]
@@ -28,8 +28,8 @@
                  [clj-time                      "0.9.0"]
                  [ring/ring-core                "1.3.2"]
                  [ring/ring-codec               "1.0.0"]
-                 [cc.qbits/alia                 "3.1.3"]
+                 [cc.qbits/alia                 "3.1.8"]
                  [cc.qbits/hayt                 "3.0.1"]
                  [cc.qbits/jet                  "0.6.2"]
-                 [net.jpountz.lz4/lz4           "1.3"]
-                 [org.xerial.snappy/snappy-java "1.1.1.7"]])
+                 [net.jpountz.lz4/lz4           "1.3.0"]
+                 [org.xerial.snappy/snappy-java "1.1.2.4"]])
